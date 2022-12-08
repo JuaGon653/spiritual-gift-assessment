7e0952b8edc05a53f7c1c4865a9ae2bc6a167465
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-12-08 14:54:44 -0600

    imports main.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Quiz from './components/Quiz.js';
+import './main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
