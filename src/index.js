aa5cd9ee3aada68c8eee97527254d671d57a5dce
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-12-07 23:42:02 -0600

    renders Quiz instead of app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Quiz from './components/Quiz.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Quiz />
     </React.StrictMode>
 );
