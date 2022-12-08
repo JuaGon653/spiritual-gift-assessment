b5bad0dc59aa443e87c4c58b70c5c7e9fefb9b13
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-12-08 17:54:20 -0600

    created routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Quiz from './components/Quiz.js';
+import HomePage from './pages/HomePage.js';
 import './main.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Quiz />
+        <Router>
+            <Routes>
+                <Route path='/quiz' element={<Quiz />} />
+                <Route path='/' element={<HomePage />} />
+            </Routes>
+        </Router>
     </React.StrictMode>
 );
