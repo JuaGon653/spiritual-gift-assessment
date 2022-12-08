e5e1e1b785d16daada1ab6a1c826e2a66ee74122
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-12-08 14:55:46 -0600

    renders questions and sorts score from high-low

---
+++ src/components/Quiz.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import data from '../data.js';
+
+const Quiz = () => {
+	const answers = new Array(64);
+
+	function onInputChange(e) {
+		answers[e.target.name - 1] = parseInt(e.target.parentNode.textContent);
+	};
+	function onSubmit(event) {
+		if (answers.includes(undefined)) {
+			console.log('please fill out all questions');
+			return;
+		}
+		function findLargest3(array) {
+			console.log(array);
+			array.sort((a, b) => parseInt(a.substring(2)) < parseInt(b.substring(2)) ? 1 : parseInt(a.substring(2)) > parseInt(b.substring(2)) ? -1 : 0);
+			console.log(array);
+		}
+		const highestToLowest = new Array(16)
+		const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
+
+		for (let i = 0; i < highestToLowest.length; i++) {
+			let aIndex = Math.trunc(i / 4); 
+			highestToLowest[i] = `${alphabet[aIndex]} ${answers[i] + answers[i+16] + answers[i+32] + answers[i+48]}` 
+		}
+		findLargest3(highestToLowest);
+	}
+
+   return (
+      <>
+		<ul className='p-4'>
+			{data.map((question) => {
+				return <li className='list-group-item pb-2' key={question.questionNum}>
+					<div>
+						<h5>{question.question}</h5>
+						<div className='ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3'>
+							<label className="options">0
+								<input type="radio" name={`${question.questionNum}`}onChange={onInputChange}/>
+							</label>
+							<label className="options">1
+								<input type="radio" name={`${question.questionNum}`} onChange={onInputChange}/>
+							</label>
+							<label className="options">2
+								<input type="radio" name={`${question.questionNum}`} onChange={onInputChange}/>
+							</label>
+							<label className="options">3
+								<input type="radio" name={`${question.questionNum}`} onChange={onInputChange}/>
+							</label>
+						</div>
+					</div>
+					<hr></hr>
+				</li>
+			})}
+		</ul>
+		<button onClick={onSubmit}>Submit</button>
+      </>
+   )
+};
+
+export default Quiz;
