acfe991a70b8c74bb02d2a5cafe2889ce9c429e4
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-12-08 19:19:09 -0600

    added basename to router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={window.location.pathname || ''}>
             <Routes>
                 <Route path='/quiz' element={<Quiz />} />
                 <Route path='/' element={<HomePage />} />
